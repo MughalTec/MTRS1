fc27c51ac72c6e06efeaee150bf1d0e0b5e6d07c
HoorulainZaheer <kashmalazaheer05@gmail.com>
2026-04-16 17:57:45 +0500

    added and modified files

--- retail_services/server.dart
+++ retail_services/server.dart
@@ -1,15 +1,15 @@
-import 'package:http/http.dart';
-import 'package:postgres/postgres.dart';
+import 'dart:io';
+import 'database_connection.dart';
 void main() async{
-  final connection = PostgreSQLConnection(
-      "remotedev.mughaltec.com",
-      5432,
-      "MTRS",
-    username:"postgres",
-      password:"",
-  );
-  await connection.open();
-  var output = await connection.query(
-    "SELECT *from mtrs.tblCompaney"
-  );
+  var server =await HttpServer.bind(InternetAddress.anyIPv4,80);
+  print('Server running on port 80');
+  var connection = await DatabaseConnection.getConnection();
+  await for (HttpRequest request in server){
+    var result = await connection.query(
+    'SELECT * FROM mtrs."tblCompaney"'
+    );
+    request.response
+    ..write(result.toString())
+    ..close();
+  }
 }
